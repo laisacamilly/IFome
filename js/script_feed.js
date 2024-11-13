4d073f00180d18ca96d2430aef3e5e70d9d97f69
Loress33 <lorenzoa@al.insper.edu.br>
2024-11-13 09:18:59 -0300

    Arrumei o negocio do like

--- js/script_feed.js
+++ js/script_feed.js
@@ -1,29 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    popup = document.querySelector(".popup");
-    enviar = popup.querySelector("#publicar");
-    main = document.querySelector("main");
+    const popup = document.querySelector(".popup");
+    const enviar = popup.querySelector("#publicar");
+    const main = document.querySelector("main");
 
     enviar.addEventListener("click", function (event) {
-        article = document.createElement("article");
+        const article = document.createElement("article");
 
-        h2 = document.createElement("h2");
-        nome = localStorage.getItem("nome");
-        if (nome) {
-            h2.innerHTML = nome;
-        } else {
-            h2.innerHTML = "Anônimo";
-        }
-
+        const h2 = document.createElement("h2");
+        const nome = localStorage.getItem("nome");
+        h2.innerHTML = nome ? nome : "Anônimo";
         article.appendChild(h2);
 
         // Obtém o arquivo do input[type='file']
-        fileInput = document.querySelector("input[type='file']");
-        file = fileInput.files[0];
+        const fileInput = document.querySelector("input[type='file']");
+        const file = fileInput.files[0];
 
         if (file) {
             // Cria uma URL temporária para o arquivo
-            foto = document.createElement("img");
+            const foto = document.createElement("img");
             foto.classList.add("comida");
             foto.src = URL.createObjectURL(file); // Define a URL temporária como src
             foto.alt = "Imagem 1";
@@ -35,48 +30,31 @@ document.addEventListener("DOMContentLoaded", function () {
             };
         }
 
-        p = document.createElement("p");
+        const p = document.createElement("p");
         p.innerHTML = document.querySelector("input[type='text']").value;
         article.appendChild(p);
 
-        a = document.createElement("a");
+        const a = document.createElement("a");
         a.href = "#";
 
-        img_like = document.createElement("img");
+        const img_like = document.createElement("img");
         img_like.src = "img/botoes.png";
         img_like.alt = "botão do like";
         img_like.classList.add("like");
         a.appendChild(img_like);
 
-        article.appendChild(a);
+        // Adiciona o evento de clique no botão "like" criado
+        img_like.addEventListener("click", function () {
+            if (img_like.getAttribute("src") === "img/botoes.png") {
+                img_like.src = "img/botoes_click.png";
+            } else {
+                img_like.src = "img/botoes.png";
+            }
+        });
 
+        article.appendChild(a);
         main.appendChild(article);
 
         event.stopPropagation();
-
-        img_like = document.querySelectorAll(".like");
-
-        img_like.forEach(function (element) {
-            element.addEventListener("click", function (event) {
-                if (element.getAttribute("src") == "img/botoes.png") {
-                    element.src = "img/botoes_click.png";
-                } else {
-                    element.src = "img/botoes.png";
-                }
-            });
-        });
-    });
-
-    img_like2 = document.querySelectorAll(".like");
-
-    img_like2.forEach(function (element) {
-        element.addEventListener("click", function (event) {
-            if (element.getAttribute("src") == "img/botoes.png") {
-                element.src = "img/botoes_click.png";
-            } else {
-                element.src = "img/botoes.png";
-            }
-
-        });
     });
 });
